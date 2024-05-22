bc4421afd11511502b9bb35cc0ccd0857353a08b
Sebas0990 <88167644+Sebas0990@users.noreply.github.com>
2024-05-22 12:22:05 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -9,6 +9,7 @@ class Recommender:
         self.prices = []
 
     def eclat(self, transactions, minsup_count):
+        print("eclat")
         item_tidsets = defaultdict(set)
         for tid, transaction in enumerate(transactions):
             for item in transaction:
@@ -33,6 +34,7 @@ class Recommender:
         self.frequent_itemsets = frequent_itemsets
 
     def calculate_supports(self, D, X, Y=None):
+        print("calculate_sup")
         count_X, count_XY, count_Y = 0, 0, 0 if Y else None
         for transaction in D:
             has_X = set(X).issubset(transaction)
@@ -49,6 +51,7 @@ class Recommender:
         return sup_X, sup_XY, sup_Y
     
     def createAssociationRules(self, F, minconf, transactions):
+        print("CreateASSO")
         B = defaultdict(list)
         itemset_support = {frozenset(itemset): support for itemset, support in F}
         for itemset, support in F:
@@ -67,6 +70,7 @@ class Recommender:
         return B
 
     def train(self, prices, database, minsup_count=10, minconf=0.1):
+        print("training")
         self.database = database
         self.prices = prices
         self.eclat(database, minsup_count)
@@ -74,6 +78,10 @@ class Recommender:
         return self
     
     def get_recommendations(self, cart, max_recommendations=5):
+        print("recommendations")
+        print(cart)
+        normalized_prices = self.prices
+
         recommendations = {}
         for antecedent, rules in self.RULES.items():
             if antecedent.issubset(cart):
@@ -88,3 +96,4 @@ class Recommender:
         avg_recommendations = {item: sum(scores) / len(scores) for item, scores in recommendations.items()}
         sorted_recommendations = sorted(avg_recommendations.items(), key=lambda x: x[1], reverse=True)
         return [item for item, _ in sorted_recommendations[:max_recommendations]]
+
