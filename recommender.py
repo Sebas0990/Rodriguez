c855ccb5c8dab54e05ff976a199065224ea53679
Sebas0990 <88167644+Sebas0990@users.noreply.github.com>
2024-05-22 11:33:57 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -3,69 +3,96 @@ from collections import defaultdict
 
 class Recommender:
     def __init__(self):
-    self.data = {
-        'RULES': [],
-        'database': [],
-        'prices': []
-    }
+        self.RULES = defaultdict(list)
+        self.frequent_itemsets = None
+        self.database = []
+        self.prices = []
 
     def eclat(self, transactions, minsup_count):
+        print("eclat")
         item_tidsets = defaultdict(set)
         for tid, transaction in enumerate(transactions):
             for item in transaction:
                 item_tidsets[item].add(tid)
 
-        def recursive_eclat(prefix, items_tidsets):
-            for item, tidset in items_tidsets.items():
-                if len(tidset) >= minsup_count:
-                    new_itemset = prefix + [item]
-                    self.frequent_itemsets.append((new_itemset, len(tidset)))
-                    suffix_tidsets = {next_item: next_tidset & tidset for next_item, next_tidset in items_tidsets.items() if next_item > item}
-                    recursive_eclat(new_itemset, suffix_tidsets)
+        item_tidsets = {item: tids for item, tids in item_tidsets.items() if len(tids) >= minsup_count}
 
-        recursive_eclat([], item_tidsets)
-        return self.frequent_itemsets
+        def eclat_recursive(prefix, items_tidsets, frequent_itemsets):
+            sorted_items = sorted(items_tidsets.items(), key=lambda x: len(x[1]), reverse=True)
+            for i, (item, tidset_i) in enumerate(sorted_items):
+                new_itemset = prefix + (item,)
+                frequent_itemsets.append((new_itemset, len(tidset_i)))
+                suffix_tidsets = {}
+                for item_j, tidset_j in sorted_items[i + 1:]:
+                    new_tidset = tidset_i & tidset_j
+                    if len(new_tidset) >= minsup_count:
+                        suffix_tidsets[item_j] = new_tidset
+                eclat_recursive(new_itemset, suffix_tidsets, frequent_itemsets)
 
-    def calculate_confidence(self, antecedent_support, support):
-    if antecedent_support > 0:
-        return support / antecedent_support
-    else:
-        return 0
+        frequent_itemsets = []
+        eclat_recursive(tuple(), item_tidsets, frequent_itemsets)
+        self.frequent_itemsets = frequent_itemsets
 
-    def create_association_rules(self, frequent_itemsets, minconf):
-    rules = []
-    itemset_support = {frozenset(itemset): support for itemset, support in frequent_itemsets}
-    for itemset, support in frequent_itemsets:
-        if len(itemset) > 1:
-            for i, antecedent in enumerate(itemset):
-                consequent = frozenset(itemset[:i] + itemset[i+1:])
-                antecedent_support = itemset_support.get(frozenset([antecedent]), 0)
-                confidence = self.calculate_confidence(antecedent_support, support)
-                if confidence >= minconf:
-                    rules.append((frozenset([antecedent]), consequent, {'confidence': confidence}))
-    return rules
+    def calculate_supports(self, D, X, Y=None):
+        print("calculate_sup")
+        count_X, count_XY, count_Y = 0, 0, 0 if Y else None
+        for transaction in D:
+            has_X = set(X).issubset(transaction)
+            has_Y = set(Y).issubset(transaction) if Y else False
+            if has_X:
+                count_X += 1
+                if Y and has_Y:
+                    count_XY += 1
+            if Y and has_Y:
+                count_Y += 1
+        sup_X = count_X / len(D)
+        sup_XY = count_XY / len(D)
+        sup_Y = count_Y / len(D) if Y is not None else None
+        return sup_X, sup_XY, sup_Y
+    
+    def createAssociationRules(self, F, minconf, transactions):
+        print("CreateASSO")
+        B = defaultdict(list)
+        itemset_support = {frozenset(itemset): support for itemset, support in F}
+        for itemset, support in F:
+            if len(itemset) > 1:
+                for i in range(len(itemset)):
+                    antecedent = frozenset([itemset[i]])
+                    consequent = frozenset(itemset[:i] + itemset[i+1:])
+                    antecedent_support = itemset_support.get(antecedent, 0)
+                    if antecedent_support > 0:
+                        conf = support / antecedent_support
+                        if conf >= minconf:
+                            metrics = {
+                                'confidence': conf  
+                            }
+                            B[antecedent].append((consequent, metrics))
+        return B
 
     def train(self, prices, database, minsup_count=10, minconf=0.1):
-    self.database = database
-    self.prices = prices
-    frequent_itemsets = self.eclat(database, minsup_count)
-    self.RULES = self.create_association_rules(frequent_itemsets, minconf)
-    return self
+        print("training")
+        self.database = database
+        self.prices = prices
+        self.eclat(database, minsup_count)
+        self.RULES = self.createAssociationRules(self.frequent_itemsets, minconf=minconf, transactions=self.database)
+        return self
     
     def get_recommendations(self, cart, max_recommendations=5):
-    normalized_prices = self.prices
-    recommendations = defaultdict(float)  # Usamos un defaultdict de tipo float para simplificar los cálculos de suma y promedio
-    for antecedent, consequent, metrics in self.RULES:
-        if antecedent.issubset(cart):
-            for item in consequent:
-                if item not in cart:
-                    price_factor = normalized_prices.get(item, 0)  # Utilizamos get para manejar casos en los que el ítem no tenga precio
-                    metric_factor = metrics['confidence']
-                    score = metric_factor * (1 + price_factor)
-                    recommendations[item] += score
-    
-    avg_recommendations = {item: score / len(self.RULES) for item, score in recommendations.items()}  # Dividimos por la cantidad total de reglas
-    sorted_recommendations = sorted(avg_recommendations.items(), key=lambda x: x[1], reverse=True)[:max_recommendations]
-    return sorted_recommendations
+        print("recommendations")
+        print(cart)
+        normalized_prices = self.prices
 
+        recommendations = {}
+        for antecedent, rules in self.RULES.items():
+            if antecedent.issubset(cart):
+                for consequent, metrics in rules:
+                    for item in consequent:
+                        if item not in cart:
+                            price_factor = normalized_prices[item] if item < len(normalized_prices) else 0
+                            metric_factor = metrics['confidence']
+                            score = metric_factor * (1 + price_factor)
+                            recommendations[item] = recommendations.get(item, []) + [score]
+
+        avg_recommendations = {item: sum(scores) / len(scores) for item, scores in recommendations.items()}
+        sorted_recommendations = sorted(avg_recommendations.items(), key=lambda x: x[1], reverse=True)
         return [item for item, _ in sorted_recommendations[:max_recommendations]]
