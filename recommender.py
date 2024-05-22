1b45b0eb20090ed296657f129dda342f0419eed0
Sebas0990 <88167644+Sebas0990@users.noreply.github.com>
2024-05-21 21:10:45 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -4,7 +4,6 @@ from collections import defaultdict
 class Recommender:
     def __init__(self):
         self.RULES = []
-        self.frequent_itemsets = None
         self.database = []
         self.prices = []
 
@@ -30,74 +29,44 @@ class Recommender:
 
         frequent_itemsets = []
         eclat_recursive(tuple(), item_tidsets, frequent_itemsets)
-        self.frequent_itemsets = frequent_itemsets
+        return frequent_itemsets
 
-    def calculate_supports(self, D, X, Y=None):
-        count_X, count_XY, count_Y = 0, 0, 0 if Y else None
-        for transaction in D:
-            has_X = set(X).issubset(transaction)
-            has_Y = set(Y).issubset(transaction) if Y else False
-            if has_X:
-                count_X += 1
-                if Y and has_Y:
-                    count_XY += 1
-            if Y and has_Y:
-                count_Y += 1
-        sup_X = count_X / len(D)
-        sup_XY = count_XY / len(D)
-        sup_Y = count_Y / len(D) if Y is not None else None
-        return sup_X, sup_XY, sup_Y
-    
-    def createAssociationRules(self, F, minconf, transactions):
-        B = []
-        itemset_support = {frozenset(itemset): support for itemset, support in F}
-        for itemset, support in F:
+    def calculate_confidence(self, antecedent_support, support):
+        return support / antecedent_support if antecedent_support > 0 else 0
+
+    def create_association_rules(self, frequent_itemsets, minconf):
+        rules = []
+        itemset_support = {frozenset(itemset): support for itemset, support in frequent_itemsets}
+        for itemset, support in frequent_itemsets:
             if len(itemset) > 1:
                 for i in range(len(itemset)):
                     antecedent = frozenset([itemset[i]])
                     consequent = frozenset(itemset[:i] + itemset[i+1:])
                     antecedent_support = itemset_support.get(antecedent, 0)
-                    if antecedent_support > 0:
-                        conf = support / antecedent_support
-                        if conf >= minconf:
-                            metrics = {
-                                'confidence': conf  
-                            }
-                            B.append((antecedent, consequent, metrics))
-        return B
+                    confidence = self.calculate_confidence(antecedent_support, support)
+                    if confidence >= minconf:
+                        rules.append((antecedent, consequent, {'confidence': confidence}))
+        return rules
 
-    def train(self, prices, database):
+    def train(self, prices, database, minsup_count=10, minconf=0.1):
         self.database = database
         self.prices = prices
-        minsup_count = 10
-        self.eclat(database, minsup_count)
-        self.RULES = self.createAssociationRules(self.frequent_itemsets, minconf=0.1, transactions=self.database)
+        frequent_itemsets = self.eclat(database, minsup_count)
+        self.RULES = self.create_association_rules(frequent_itemsets, minconf)
         return self
     
     def get_recommendations(self, cart, max_recommendations=5):
         normalized_prices = self.prices
-
         recommendations = defaultdict(list)
         for rule in self.RULES:
-            if rule[0].issubset(cart):  
-                for item in rule[1]:  
-                    if item not in cart:  
+            if rule[0].issubset(cart):
+                for item in rule[1]:
+                    if item not in cart:
                         price_factor = normalized_prices[item] if item < len(normalized_prices) else 0
-                        metric_factor = rule[2]['confidence']  
-                        score = metric_factor * (1 + price_factor)  
-                        recommendations[item].append(score)  
+                        metric_factor = rule[2]['confidence']
+                        score = metric_factor * (1 + price_factor)
+                        recommendations[item].append(score)
         
         avg_recommendations = {item: sum(scores) / len(scores) if len(scores) > 0 else 0 for item, scores in recommendations.items()}
         sorted_recommendations = sorted(avg_recommendations.items(), key=lambda x: x[1], reverse=True)
         return [item for item, _ in sorted_recommendations[:max_recommendations]]
-
-# Ejemplo de uso
-if __name__ == "__main__":
-    recommender = Recommender()
-    prices = [10, 20, 30, 15, 25]
-    database = [[1, 2, 3], [1, 2, 4], [1, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]]
-    recommender.train(prices, database)
-    cart = [1, 2]
-    recommendations = recommender.get_recommendations(cart)
-    print("Recomendaciones:", recommendations)
-
